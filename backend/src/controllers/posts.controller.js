c9c8db0afaf58fe98791809818944abb3f12eda6
Alejandro Sosa <agsosa74@gmail.com>
2021-05-18 16:20:52 -0300

    Add update controller

--- backend/src/controllers/posts.controller.js
+++ backend/src/controllers/posts.controller.js
@@ -2,6 +2,7 @@ const { Sequelize } = require('sequelize');
 const postModel = require('@models/post.model');
 const helpers = require('@lib/helpers');
 
+// DELETE api/posts/:id
 exports.delete = async function (req, res) {
   try {
     // Parse ID param
@@ -13,15 +14,16 @@ exports.delete = async function (req, res) {
 
     // If we get result = 0, then no post was deleted
     if (!result) throw new Error('POST_NOT_FOUND');
-    else helpers.sendSuccessResponse(res, 'POST_DELETED', result);
+    else helpers.sendSuccessResponse(res, 'POST_DELETED', {});
   } catch (error) {
     helpers.sendFailedResponse(res, error, 500);
   }
 };
 
+// GET api/posts
 exports.findAll = async function (req, res) {
   try {
-    const result = await postModel.findAll({ include: 'category' }); // result: array of postModel objects
+    const result = await postModel.findAll({ include: 'category', attributes: { exclude: ['categoryId', 'content'] } }); // result: array of postModel objects
 
     helpers.sendSuccessResponse(res, 'POSTS_LIST', result);
   } catch (error) {
@@ -29,8 +31,48 @@ exports.findAll = async function (req, res) {
   }
 };
 
-exports.update = async function (req, res) {};
+// PATCH api/posts/:id
+exports.update = async function (req, res) {
+  try {
+    // Parse post ID param
+    const postId = Number.parseInt(req.params.id);
+    if (Number.isNaN(postId)) throw new Error('INVALID_POST_ID');
 
+    // Build update params object
+    const updateParams = {};
+
+    // Get title & content
+    const { title, content, category_id } = req.body;
+    if (title) updateParams.title = title;
+    if (content) updateParams.content = content;
+
+    if (category_id) {
+      const categoryId = Number.parseInt(req.body.category_id);
+      if (Number.isNaN(categoryId)) throw new Error('INVALID_CATEGORY_ID');
+
+      updateParams.categoryId = categoryId;
+    }
+
+    // TODO: Add image field param
+
+    // Execute model method
+    const result = await postModel.update(updateParams, { where: { id: postId } }); // result: array of numbers
+
+    // If we get !result, then no post was created
+    if (!result || result.length === 0 || result[0] === 0) throw new Error('POST_NOT_FOUND');
+    else {
+      helpers.sendSuccessResponse(res, 'POST_UPDATED', {});
+    }
+  } catch (error) {
+    if (error instanceof Sequelize.ForeignKeyConstraintError && error.fields.includes('categoryId')) {
+      error = 'CATEGORY_NOT_FOUND';
+    }
+
+    helpers.sendFailedResponse(res, error, 500);
+  }
+};
+
+// POST api/posts
 exports.create = async function (req, res) {
   try {
     // Parse category ID
@@ -45,7 +87,10 @@ exports.create = async function (req, res) {
     // TODO: Accept image param
 
     // Execute model method
-    const result = await postModel.create({ title, content, categoryId }, { include: 'category' }); // result: object of postModel
+    const result = await postModel.create({ title, content, categoryId }); // result: object of postModel
+
+    result.dataValues.category = await result.getCategory();
+    delete result.dataValues.categoryId;
 
     // If we get !result, then no post was created
     if (!result) throw new Error('POST_NOT_CREATED');
@@ -59,6 +104,7 @@ exports.create = async function (req, res) {
   }
 };
 
+// GET /api/posts/:id
 exports.findOne = async function (req, res) {
   try {
     // Parse ID param
@@ -66,7 +112,11 @@ exports.findOne = async function (req, res) {
     if (Number.isNaN(id)) throw new Error('INVALID_POST_ID');
 
     // Execute model method
-    const result = await postModel.findOne({ where: { id: Number(req.params.id) }, include: 'category' }); // result: object of postModel
+    const result = await postModel.findOne({
+      where: { id: Number(req.params.id) },
+      include: 'category',
+      attributes: { exclude: ['categoryId'] },
+    }); // result: object of postModel
 
     // If we get !result, then no post was found
     if (!result) throw new Error('POST_NOT_FOUND');
