88702f7dd1ff08f9cfb0ba52a0ba9427c9d98dee
Alejandro Sosa <agsosa74@gmail.com>
2021-05-18 16:20:56 -0300

    Update category.model.js

--- backend/src/models/category.model.js
+++ backend/src/models/category.model.js
@@ -10,19 +10,26 @@ const db = require('@lib/database');
    - updatedAt (modified date)
    - relations (foreign keys) fields are added automatically by sequelize
 */
-const model = db.instance.define('categories', {
-  locale_es: {
-    // Spanish translation
-    type: DataTypes.STRING,
-    allowNull: false,
-    defaultValue: 'NO_TRANSLATION',
+const model = db.instance.define(
+  'categories',
+  {
+    locale_es: {
+      // Spanish translation
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'NO_TRANSLATION',
+    },
+    locale_en: {
+      // English translation
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'NO_TRANSLATION',
+    },
   },
-  locale_en: {
-    // English translation
-    type: DataTypes.STRING,
-    allowNull: false,
-    defaultValue: 'NO_TRANSLATION',
-  },
-});
+  {
+    // Disable updatedAt, createdAt fields for this model
+    timestamps: false,
+  }
+);
 
 module.exports = model;
