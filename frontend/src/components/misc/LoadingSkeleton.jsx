a616424c327a3e91e1ac56ba34951881e2b5b24d
Alejandro Sosa <agsosa74@gmail.com>
2021-05-19 15:01:21 -0300

    Create LoadingSkeleton.jsx

---
+++ frontend/src/components/misc/LoadingSkeleton.jsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import Skeleton from '@material-ui/lab/Skeleton';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  loadingContainer: {
+    width: '100%',
+  },
+});
+
+export default function ({ loading }) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.loadingContainer}>
+      {Array.from(Array(10).keys()).map(() => (
+        <Skeleton height={50} />
+      ))}
+    </div>
+  );
+}
