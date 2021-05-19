e9a946a7e9aeda6aa5aca0fbd0c45f20caeae85b
Alejandro Sosa <agsosa74@gmail.com>
2021-05-19 14:11:38 -0300

    Animate container

--- frontend/src/components/layout/Container.jsx
+++ frontend/src/components/layout/Container.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
+import { motion } from 'framer-motion';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,6 +14,10 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(3),
     height: '100%',
   },
+  animContainer: {
+    width: '100%',
+    height: '100%',
+  },
 }));
 
 export default function ({ children, ...props }) {
@@ -20,7 +25,13 @@ export default function ({ children, ...props }) {
 
   return (
     <Container maxWidth='xl' className={styles.root} {...props}>
-      {children}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        className={styles.animContainer}>
+        {children}
+      </motion.div>
     </Container>
   );
 }
